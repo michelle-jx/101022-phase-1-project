b401c2ec3ed0b485b629ae7c91a5d2f12ec18f94
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-24 12:18:42 -0700

    added fetch and display functions

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ const API = {
 
 const apiURL = `${API.url}${API.type}/${API.id}`
 console.log(apiURL)
+
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+   .then(res => res.json())
+   .then(drinkData => {
+    // console.log(drinkData.drinks[0].strDrinks)
+    renderDrinks(drinkData)
+   })
+
+const renderDrinks = (drinkData) => {
+    drinkData.drinks.forEach(displayDrinks)
+    console.log(drinkData)
+}
+
+
+
+const displayDrinks = (drinkData) => { 
+
+    // console.log(drinkData.drinks[0].strDrink)
+
+    const drinkUl = document.getElementById('name-list')
+
+    const drinkLi = document.createElement('li')
+
+    drinkLi.textContent = drinkData.strTags
+
+    drinkUl.append(drinkLi)
+
+}
+
+
+
