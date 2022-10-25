878b0e6a731c011260770ccbe23fb8dfbfcc0614
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-25 16:20:57 -0700

    refactored code creating ul tags and added if statment to remove null

--- index.js
+++ index.js
@@ -1,139 +1,153 @@
-// const API = {
-//     url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
-//     type: 'cocktail',
-//     id: '11007',
-// }   
+const API = {
+    url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
+    type: 'cocktail',
+    id: '11007',
+}   
 
-// const apiURL = `${API.url}${API.type}/${API.id}`
-// // console.log(apiURL)
-
-
+const apiURL = `${API.url}${API.type}/${API.id}`
+// console.log(apiURL)
 
 
 
 
 
-// const drinkUl = document.getElementById('name-list')
 
 
-// fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
-//    .then(res => res.json())
-//    .then(drinkData => {
-//     // console.log(drinkData.drinks[0].strDrinks)
-//     renderDrinks(drinkData)
+const drinkUl = document.getElementById('name-list')
+
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+   .then(res => res.json())
+   .then(drinkData => {
+    // console.log(drinkData.drinks[0].strDrinks)
+    renderDrinks(drinkData)
   
-//    })
+   })
 
-// const renderDrinks = (drinkData) => {
-//     drinkData.drinks.forEach(displayDrinks)
-
-   
-//     console.log(drinkData)
-// }
-
-
-
-// const displayDrinks = (drinkData) => { 
-
-//     // console.log(drinkData.drinks[0].strDrink)
+const renderDrinks = (drinkData) => {
+    drinkData.drinks.forEach(displayDrinks)
 
    
+    console.log(drinkData)
+}
+
+
+
+const displayDrinks = (drinkData) => { 
+
+    // console.log(drinkData.drinks[0].strDrink
+    const drinkName = document.createElement('h2')
+    const drinkInst = document.createElement('p')
+    const drinkCategory = document.createElement('h3')
+    const drinkGlass = document.createElement('h4')
+    const drinkImg = document.createElement('img')
+
+    for(let i = 1; i <16; i++) {
+        console.log(i)
+
+        if(drinkData[`strIngredient${i}`] == null) {
+            break
+        }
+
+
+        const ingredient = document.createElement('ul')
+        ingredient.innerHTML =  drinkData[`strMeasure${i}`] + drinkData[`strIngredient${i}`]
 
     
 
-//     const drinkName = document.createElement('h2')
-//     const drinkInst = document.createElement('p')
-//     const drinkCategory = document.createElement('h3')
-//     const drinkGlass = document.createElement('h4')
-//     const drinkImg = document.createElement('img')
 
-//     const ingredient1 = document.createElement('ul')
-//     const ingredient2 = document.createElement('ul')
-//     const ingredient3 = document.createElement('ul')
-//     const ingredient4 = document.createElement('ul')
-//     const ingredient5 = document.createElement('ul')
-//     const ingredient6 = document.createElement('ul')
-//     const ingredient7 = document.createElement('ul')
-//     const ingredient8 = document.createElement('ul')
+        drinkUl.appendChild(ingredient)
 
-//     const measure1 = document.createElement('ul')
-//     const measure2 = document.createElement('ul')
-//     const measure3 = document.createElement('ul')
-//     const measure4 = document.createElement('ul')
-//     const measure5 = document.createElement('ul')
-//     const measure6 = document.createElement('ul')
-//     const measure7 = document.createElement('ul')
-//     const measure8 = document.createElement('ul')
+    }
 
-//     if (drinkData === null) {
-//         drinkUl.textContent = '' 
-//     }
+    // const ingredient1 = document.createElement('ul')
+    // const ingredient2 = document.createElement('ul')
+    // const ingredient3 = document.createElement('ul')
+    // const ingredient4 = document.createElement('ul')
+    // const ingredient5 = document.createElement('ul')
+    // const ingredient6 = document.createElement('ul')
+    // const ingredient7 = document.createElement('ul')
+    // const ingredient8 = document.createElement('ul')
+
+    // const measure1 = document.createElement('ul')
+    // const measure2 = document.createElement('ul')
+    // const measure3 = document.createElement('ul')
+    // const measure4 = document.createElement('ul')
+    // const measure5 = document.createElement('ul')
+    // const measure6 = document.createElement('ul')
+    // const measure7 = document.createElement('ul')
+    // const measure8 = document.createElement('ul')
+
+    // if (drinkData === null) {
+    //     drinkUl.textContent = '' 
+    // }
 
  
-//     drinkGlass.textContent = `Glass type: ${drinkData.strGlass}`
-//     drinkImg.src = drinkData.strDrinkThumb
-//     drinkImg.alt = drinkData.strDrink
-//     drinkInst.textContent = `Instructions: ${drinkData.strInstructions}`
-//     drinkCategory.textContent = `This is a ${drinkData.strCategory}`
-//     drinkName.textContent = `Name: ${drinkData.strDrink}`
+    drinkGlass.textContent = `Glass type: ${drinkData.strGlass}`
+    drinkImg.src = drinkData.strDrinkThumb
+    drinkImg.alt = drinkData.strDrink
+    drinkInst.textContent = `Instructions: ${drinkData.strInstructions}`
+    drinkCategory.textContent = `This is a ${drinkData.strCategory}`
+    drinkName.textContent = `Name: ${drinkData.strDrink}`
 
-//     // if ( typeof(drinkData) === "null") {
-//     //     drinkUl.innerHTML = ''
-//     // }
+    // if ( typeof(drinkData) === "null") {
+    //     drinkUl.innerHTML = ''
+    // }
 
-//     // ingredient1.textContent = drinkData.strIngredient1
-//     // ingredient2.textContent = drinkData.strIngredient2
-//     // ingredient3.textContent = drinkData.strIngredient3
-//     // ingredient4.textContent = drinkData.strIngredient4
-//     // ingredient5.textContent = drinkData.strIngredient5
-//     // ingredient6.textContent = drinkData.strIngredient6
-//     // ingredient7.textContent = drinkData.strIngredient7
-//     // ingredient8.textContent = drinkData.strIngredient8 
+    // ingredient1.textContent = drinkData.strIngredient1
+    // ingredient2.textContent = drinkData.strIngredient2
+    // ingredient3.textContent = drinkData.strIngredient3
+    // ingredient4.textContent = drinkData.strIngredient4
+    // ingredient5.textContent = drinkData.strIngredient5
+    // ingredient6.textContent = drinkData.strIngredient6
+    // ingredient7.textContent = drinkData.strIngredient7
+    // ingredient8.textContent = drinkData.strIngredient8 
 
 
-//     // measure1.textContent = drinkData.strMeasure1
-//     // measure2.textContent = drinkData.strMeasure2
-//     // measure3.textContent = drinkData.strMeasure3
-//     // measure4.textContent = drinkData.strMeasure4
-//     // measure5.textContent = drinkData.strMeasure5
-//     // measure6.textContent = drinkData.strMeasure6
-//     // measure7.textContent = drinkData.strMeasure7
-//     // measure8.textContent = drinkData.strMeasure8
+    // measure1.textContent = drinkData.strMeasure1
+    // measure2.textContent = drinkData.strMeasure2
+    // measure3.textContent = drinkData.strMeasure3
+    // measure4.textContent = drinkData.strMeasure4
+    // measure5.textContent = drinkData.strMeasure5
+    // measure6.textContent = drinkData.strMeasure6
+    // measure7.textContent = drinkData.strMeasure7
+    // measure8.textContent = drinkData.strMeasure8
 
-//     ingredient1.textContent= `${drinkData.strIngredient1} ${drinkData.strMeasure1}`
-//     ingredient2.textContent = `${drinkData.strIngredient2} ${drinkData.strMeasure2}`
-//     ingredient3.textContent = `${drinkData.strIngredient3} ${drinkData.strMeasure3}`
-//     ingredient4.textContent = `${drinkData.strIngredient4} ${drinkData.strMeasure4}`
-//     ingredient5.textContent = `${drinkData.strIngredient5} ${drinkData.strMeasure5}`
-//     ingredient6.textContent = `${drinkData.strIngredient6} ${drinkData.strMeasure6}`
-//     ingredient7.textContent = `${drinkData.strIngredient7} ${drinkData.strMeasure7}`
-//     ingredient8.textContent = `${drinkData.strIngredient8} ${drinkData.strMeasure8}`
 
-//     drinkUl.append(drinkName, 
-//         ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,  ingredient6,  ingredient7,  ingredient8,
-//         measure1, measure2, measure3, measure4, measure5, measure6, measure7, measure8,
-//          drinkInst, drinkGlass, drinkImg, drinkCategory 
+    // ingredient1.textContent=  `${drinkData.strMeasure1}``${drinkData.strIngredient1}` 
+    // ingredient2.textContent = `${drinkData.strMeasure2}``${drinkData.strIngredient2}`
+    // ingredient3.textContent = `${drinkData.strMeasure3}``${drinkData.strIngredient3}`
+    // ingredient4.textContent = `${drinkData.strMeasure4}``${drinkData.strIngredient4}`
+    // ingredient5.textContent = `${drinkData.strMeasure5}``${drinkData.strIngredient5}`
+    // ingredient6.textContent = `${drinkData.strMeasure6}``${drinkData.strIngredient6}`
+    // ingredient7.textContent = `${drinkData.strMeasure7}``${drinkData.strIngredient7}`
+    // ingredient8.textContent = `${drinkData.strMeasure8}``${drinkData.strIngredient8}`
 
-//          )
+     drinkUl.append(drinkName,
+    //     ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,  ingredient6,  ingredient7,  ingredient8,
+    //     measure1, measure2, measure3, measure4, measure5, measure6, measure7, measure8,
+         drinkInst, drinkGlass, drinkImg, drinkCategory 
+// 
+         )
 
-//         //  if ( typeof(drinkData) === "null") {
-//         //     drinkUl.innerHTML = ''
-//         // }
+        //  if ( typeof(drinkData) === "null") {
+        //     drinkUl.innerHTML = ''
+        // }
     
   
 
-// }
+}
 
 
 
 
-//DRINK RANDOMIZER
+// DRINK RANDOMIZER
 
 
 
 const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-const rDrinkImg = document.createElement('img')
+
 
 const randomizerDiv = document.getElementById('random-drink');
 const rButton = document.getElementById('random-btn');
@@ -145,7 +159,7 @@ const getRandomDrink = () => {
         .then( res => res.json())
         .then( drink => {
             console.log(drink)
-            rDrinkImg.style.visibility = 'visible'
+            // rDrinkImg.style.visibility = 'visible'
 
             renderDrink(drink)
             
@@ -166,6 +180,7 @@ const displayRDrink = (drink) => {
 
   
     const rDrinkName = document.createElement('name')
+    const rDrinkImg = document.createElement('img')
 
     // rDrinkImg.style.visibility = 'hidden'
 
@@ -175,16 +190,19 @@ const displayRDrink = (drink) => {
     rDrinkImg.src = drink.strDrinkThumb
 
     randomizerDiv.append(rDrinkName, rDrinkImg)
-    
+
     rButton.addEventListener('click', getRandomDrink)
 
-    // rDrinkImg.style.visibility = 'hidden'
+   
+    };
 
-    console.log(drink.strDrink)
+
+
+    // rDrinkImg.style.visibility = 'block'
+
+    // console.log(drink.strDrink)
 
     
-
-}
 
 
 const init = () => {
