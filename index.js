8aeca659da6fb0a7b601776004bd20128e60bc3e
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-26 12:26:42 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const drinkUl = document.getElementById('name-list')
+//Creating the Form and search. 
+
 let possibleCocktails1 = [];
 let possibleCocktails2 = [];
 let possibleCocktails3 = [];
 let possibleCocktails4 = [];
 let possibleCocktails5 = [];
+
 fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
-.then(res => res.json())
-.then(drinkData => {
-// console.log(drinkData.drinks[0].strDrinks)
-renderDrinks(drinkData)
-})
+    .then(res => res.json())
+    .then(drinkData => {
+        // console.log(drinkData.drinks[0].strDrinks)
+        renderDrinks(drinkData)
+    })
+
 const renderDrinks = (drinkData) => {
-drinkData.drinks.forEach(displayDrinks)
-console.log(drinkData)
+    drinkData.drinks.forEach(displayDrinks)
+    console.log(drinkData)
 }
+
 getEnteredIngredients();
+
 function getEnteredIngredients() {
+
     const somethingToGrab = document.getElementById('create-ingredients-list');
     const ingredientsForm = document.createElement('form');
     const getFirstIngredient = document.createElement('input');
@@ -24,27 +30,33 @@ function getEnteredIngredients() {
     const getFourthIngredient = document.createElement('input');
     const getFifthIngredient = document.createElement('input');
     const submitButton = document.createElement('button');
+
     getFirstIngredient.placeholder = 'Ingredient';
     getSecondIngredient.placeholder = 'Ingredient';
     getThirdIngredient.placeholder = 'Ingredient';
     getFourthIngredient.placeholder = 'Ingredient';
     getFifthIngredient.placeholder = 'Ingredient';
     submitButton.textContent = 'Submit Ingredients';
+
     somethingToGrab.append(ingredientsForm)
     ingredientsForm.append(getFirstIngredient, getSecondIngredient, getThirdIngredient, getFourthIngredient, getFifthIngredient, submitButton);
+
     let enteredCocktailIngredients = []
+
     ingredientsForm.addEventListener('submit', (e) => {
         e.preventDefault()
-        const ingrOneSearch = e.target[0].value
-        const ingrTwoSearch = e.target[1].value
-        const ingrThreeSearch = e.target[2].value
-        const ingrFourSearch = e.target[3].value
-        const ingrFiveSearch = e.target[4].value
-         for (i=0; i<5; i++){
+        // const ingrOneSearch = e.target[0].value
+        // const ingrTwoSearch = e.target[1].value
+        // const ingrThreeSearch = e.target[2].value
+        // const ingrFourSearch = e.target[3].value
+        // const ingrFiveSearch = e.target[4].value
+
+        for (i = 0; i < 5; i++) {
             enteredCocktailIngredients.push(e.target[i].value)
         }
         console.log(enteredCocktailIngredients)
         searchIngredients(enteredCocktailIngredients)
+
         // console.log(cocktailIngredients)
         // console.log(ingrOneSearch)
         // console.log(ingrTwoSearch)
@@ -56,22 +68,25 @@ function getEnteredIngredients() {
         // searchIngredients(ingrThreeSearch)
         // searchIngredients(ingrFourSearch)
         // searchIngredients(ingrFiveSearch)
+
     })
+
 }
+
 function searchIngredients(ingrSearchArray) {
     console.log("Search Ingredients is running");
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
-    .then(res => res.json())
-    .then(drinkData => {
-    //console.log(drinkData)
-    drinkData.drinks.forEach((singleDrinkData) => {
-        getCocktailIngredients(singleDrinkData, ingrSearchArray)
-        // I tried drinkData.drinks to be passed above and it changed what was
-        // console.logged slightly, but I still couldn't access data I needed
-        // Phil clarified. I needed to pass just the one drink. Not the entire array.
-        //console.log(possibleCocktails)
-    })
-   })
+        .then(res => res.json())
+        .then(drinkData => {
+            //console.log(drinkData)
+            drinkData.drinks.forEach((singleDrinkData) => {
+                getCocktailIngredients(singleDrinkData, ingrSearchArray)
+                // I tried drinkData.drinks to be passed above and it changed what was
+                // console.logged slightly, but I still couldn't access data I needed
+                // Phil clarified. I needed to pass just the one drink. Not the entire array.
+                //console.log(possibleCocktails)
+            })
+        })
     // for (i=0; i<5; i++) {
     //     console.log('Did I run?')
     //  if (ingredient1 === ingrOneSearch){
@@ -80,9 +95,10 @@ function searchIngredients(ingrSearchArray) {
     //  }
     //  else  {console.log('Hi')
     //  }
-        //do I set it to a boolean?
+    //do I set it to a boolean?
 }
-function getCocktailIngredients(singleDrinkData, ingrSearchArray){
+function getCocktailIngredients(singleDrinkData, ingrSearchArray) {
+
     //console.log("getCocktailIngredients ran")
     //console.log(ingrSearchArray)
     //console.log(singleDrinkData)
@@ -93,24 +109,26 @@ function getCocktailIngredients(singleDrinkData, ingrSearchArray){
     //     console.log(singleDrinkData.strIngredient[i])
     //     //singleCocktailIngredients.push(singleDrinkData.strIngredient[i])
     // }
-    for (i=0; i<5; i++) {
+
+    for (i = 0; i < 5; i++) {
+
         //console.log(ingrSearchArray[i])
         //console.log(singleDrinkData[`strIngredient${i+1}`])
-        for (j=1; j<6; j++){
-            if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i===0) {
-            possibleCocktails1.push(singleDrinkData.strDrink)
+        for (j = 1; j < 6; j++) {
+            if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i === 0) {
+                possibleCocktails1.push(singleDrinkData.strDrink)
             }
-            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i===1){
-            possibleCocktails2.push(singleDrinkData.strDrink)
+            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i === 1) {
+                possibleCocktails2.push(singleDrinkData.strDrink)
             }
-            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i===2){
-            possibleCocktails3.push(singleDrinkData.strDrink)
+            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i === 2) {
+                possibleCocktails3.push(singleDrinkData.strDrink)
             }
-            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i===3){
-            possibleCocktails4.push(singleDrinkData.strDrink)
+            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i === 3) {
+                possibleCocktails4.push(singleDrinkData.strDrink)
             }
-            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i===4){
-            possibleCocktails5.push(singleDrinkData.strDrink)
+            else if (ingrSearchArray[i] === singleDrinkData[`strIngredient${j}`] && i === 4) {
+                possibleCocktails5.push(singleDrinkData.strDrink)
             }
         }
     }
@@ -119,115 +137,146 @@ function getCocktailIngredients(singleDrinkData, ingrSearchArray){
     console.log(possibleCocktails3)
     console.log(possibleCocktails4)
     console.log(possibleCocktails5)
+
 }
+
+//Dislay drink Informattion
+
+const drinkUl = document.getElementById('name-list')
+
 const displayDrinks = (drinkData) => {
-// console.log(drinkData.drinks[0].strDrink
-const drinkName = document.createElement('h2')
-const drinkInst = document.createElement('p')
-const drinkCategory = document.createElement('h3')
-const drinkGlass = document.createElement('h4')
-const drinkImg = document.createElement('img')
-for(let i = 1; i <16; i++) {
-    console.log(i)
-    if(drinkData[`strIngredient${i}`] == null) {
-        break
+
+    // console.log(drinkData.drinks[0].strDrink
+    const drinkName = document.createElement('h2')
+    const drinkInst = document.createElement('p')
+    const drinkCategory = document.createElement('h3')
+    const drinkGlass = document.createElement('h4')
+    const drinkImg = document.createElement('img')
+
+
+    for (let i = 1; i < 16; i++) {
+        console.log(i)
+        if (drinkData[`strIngredient${i}`] == null) {
+            break
+        }
+
+        const ingredient = document.createElement('ul')
+
+        ingredient.innerHTML = drinkData[`strMeasure${i}`] + drinkData[`strIngredient${i}`]
+        drinkUl.appendChild(ingredient)
     }
-    const ingredient = document.createElement('ul')
-    ingredient.innerHTML =  drinkData[`strMeasure${i}`] + drinkData[`strIngredient${i}`]
-    drinkUl.appendChild(ingredient)
-}
-// const ingredient1 = document.createElement('ul')
-// const ingredient2 = document.createElement('ul')
-// const ingredient3 = document.createElement('ul')
-// const ingredient4 = document.createElement('ul')
-// const ingredient5 = document.createElement('ul')
-// const ingredient6 = document.createElement('ul')
-// const ingredient7 = document.createElement('ul')
-// const ingredient8 = document.createElement('ul')
-// const measure1 = document.createElement('ul')
-// const measure2 = document.createElement('ul')
-// const measure3 = document.createElement('ul')
-// const measure4 = document.createElement('ul')
-// const measure5 = document.createElement('ul')
-// const measure6 = document.createElement('ul')
-// const measure7 = document.createElement('ul')
-// const measure8 = document.createElement('ul')
-// if (drinkData === null) {
-//     drinkUl.textContent = ''
-// }
-drinkGlass.textContent = `Glass type: ${drinkData.strGlass}`
-drinkImg.src = drinkData.strDrinkThumb
-drinkImg.alt = drinkData.strDrink
-drinkInst.textContent = `Instructions: ${drinkData.strInstructions}`
-drinkCategory.textContent = `This is a ${drinkData.strCategory}`
-drinkName.textContent = `Name: ${drinkData.strDrink}`
-// if ( typeof(drinkData) === "null") {
-//     drinkUl.innerHTML = ''
-// }
-// ingredient1.textContent = drinkData.strIngredient1
-// ingredient2.textContent = drinkData.strIngredient2
-// ingredient3.textContent = drinkData.strIngredient3
-// ingredient4.textContent = drinkData.strIngredient4
-// ingredient5.textContent = drinkData.strIngredient5
-// ingredient6.textContent = drinkData.strIngredient6
-// ingredient7.textContent = drinkData.strIngredient7
-// ingredient8.textContent = drinkData.strIngredient8
-// measure1.textContent = drinkData.strMeasure1
-// measure2.textContent = drinkData.strMeasure2
-// measure3.textContent = drinkData.strMeasure3
-// measure4.textContent = drinkData.strMeasure4
-// measure5.textContent = drinkData.strMeasure5
-// measure6.textContent = drinkData.strMeasure6
-// measure7.textContent = drinkData.strMeasure7
-// measure8.textContent = drinkData.strMeasure8
-// ingredient1.textContent=  `${drinkData.strMeasure1}``${drinkData.strIngredient1}`
-// ingredient2.textContent = `${drinkData.strMeasure2}``${drinkData.strIngredient2}`
-// ingredient3.textContent = `${drinkData.strMeasure3}``${drinkData.strIngredient3}`
-// ingredient4.textContent = `${drinkData.strMeasure4}``${drinkData.strIngredient4}`
-// ingredient5.textContent = `${drinkData.strMeasure5}``${drinkData.strIngredient5}`
-// ingredient6.textContent = `${drinkData.strMeasure6}``${drinkData.strIngredient6}`
-// ingredient7.textContent = `${drinkData.strMeasure7}``${drinkData.strIngredient7}`
-// ingredient8.textContent = `${drinkData.strMeasure8}``${drinkData.strIngredient8}`
- drinkUl.append(drinkName,
-//     ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,  ingredient6,  ingredient7,  ingredient8,
-//     measure1, measure2, measure3, measure4, measure5, measure6, measure7, measure8,
-     drinkInst, drinkGlass, drinkImg, drinkCategory
-//
-     )
-    //  if ( typeof(drinkData) === "null") {
+
+    drinkGlass.textContent = `Glass type: ${drinkData.strGlass}`
+    drinkImg.src = drinkData.strDrinkThumb
+    drinkImg.alt = drinkData.strDrink
+    drinkInst.textContent = `Instructions: ${drinkData.strInstructions}`
+    drinkCategory.textContent = `This is a ${drinkData.strCategory}`
+    drinkName.textContent = `Name: ${drinkData.strDrink}`
+
+    drinkUl.append(drinkName, drinkInst, drinkGlass, drinkImg, drinkCategory)
+
+    // const ingredient1 = document.createElement('ul')
+    // const ingredient2 = document.createElement('ul')
+    // const ingredient3 = document.createElement('ul')
+    // const ingredient4 = document.createElement('ul')
+    // const ingredient5 = document.createElement('ul')
+    // const ingredient6 = document.createElement('ul')
+    // const ingredient7 = document.createElement('ul')
+    // const ingredient8 = document.createElement('ul')
+    // const measure1 = document.createElement('ul')
+    // const measure2 = document.createElement('ul')
+    // const measure3 = document.createElement('ul')
+    // const measure4 = document.createElement('ul')
+    // const measure5 = document.createElement('ul')
+    // const measure6 = document.createElement('ul')
+    // const measure7 = document.createElement('ul')
+    // const measure8 = document.createElement('ul')
+    // if (drinkData === null) {
+    //     drinkUl.textContent = ''
+    // }
+
+     // if ( typeof(drinkData) === "null") {
     //     drinkUl.innerHTML = ''
     // }
+    // ingredient1.textContent = drinkData.strIngredient1
+    // ingredient2.textContent = drinkData.strIngredient2
+    // ingredient3.textContent = drinkData.strIngredient3
+    // ingredient4.textContent = drinkData.strIngredient4
+    // ingredient5.textContent = drinkData.strIngredient5
+    // ingredient6.textContent = drinkData.strIngredient6
+    // ingredient7.textContent = drinkData.strIngredient7
+    // ingredient8.textContent = drinkData.strIngredient8
+    // measure1.textContent = drinkData.strMeasure1
+    // measure2.textContent = drinkData.strMeasure2
+    // measure3.textContent = drinkData.strMeasure3
+    // measure4.textContent = drinkData.strMeasure4
+    // measure5.textContent = drinkData.strMeasure5
+    // measure6.textContent = drinkData.strMeasure6
+    // measure7.textContent = drinkData.strMeasure7
+    // measure8.textContent = drinkData.strMeasure8
+    // ingredient1.textContent=  `${drinkData.strMeasure1}``${drinkData.strIngredient1}`
+    // ingredient2.textContent = `${drinkData.strMeasure2}``${drinkData.strIngredient2}`
+    // ingredient3.textContent = `${drinkData.strMeasure3}``${drinkData.strIngredient3}`
+    // ingredient4.textContent = `${drinkData.strMeasure4}``${drinkData.strIngredient4}`
+    // ingredient5.textContent = `${drinkData.strMeasure5}``${drinkData.strIngredient5}`
+    // ingredient6.textContent = `${drinkData.strMeasure6}``${drinkData.strIngredient6}`
+    // ingredient7.textContent = `${drinkData.strMeasure7}``${drinkData.strIngredient7}`
+    // ingredient8.textContent = `${drinkData.strMeasure8}``${drinkData.strIngredient8}`
+
+     //     ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,  ingredient6,  ingredient7,  ingredient8,
+        //     measure1, measure2, measure3, measure4, measure5, measure6, measure7, measure8,
+
 }
+
+
+
 // DRINK RANDOMIZER
+
+
 const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
 const randomizerDiv = document.getElementById('random-drink');
 const rButton = document.getElementById('random-btn');
+
 const getRandomDrink = () => {
-fetch(API_URL)
-    .then( res => res.json())
-    .then( drink => {
-        console.log(drink)
-        // rDrinkImg.style.visibility = 'visible'
-        renderDrink(drink)
-        // displayDrink()
-    })
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(drink => {
+            console.log(drink)
+            // rDrinkImg.style.visibility = 'visible'
+            renderDrink(drink)
+            // displayDrink()
+        })
+
 }
+
 const renderDrink = (drink) => {
-drink.drinks.forEach(displayRDrink)
+
+    drink.drinks.forEach(displayRDrink)
 }
+
 const displayRDrink = (drink) => {
-const rDrinkName = document.createElement('name')
-const rDrinkImg = document.createElement('img')
-// rDrinkImg.style.visibility = 'hidden'
-randomizerDiv.innerHTML = ''
-rDrinkName.textContent = drink.strDrink
-rDrinkImg.src = drink.strDrinkThumb
-randomizerDiv.append(rDrinkName, rDrinkImg)
-rButton.addEventListener('click', getRandomDrink)
+
+    const rDrinkName = document.createElement('name')
+    const rDrinkImg = document.createElement('img')
+
+    // rDrinkImg.style.visibility = 'hidden'
+
+    randomizerDiv.innerHTML = ''
+
+    rDrinkName.textContent = drink.strDrink
+    rDrinkImg.src = drink.strDrinkThumb
+    randomizerDiv.append(rDrinkName, rDrinkImg)
+    rButton.addEventListener('click', getRandomDrink)
+
 };
+
 // rDrinkImg.style.visibility = 'block'
 // console.log(drink.strDrink)
 const init = () => {
-getRandomDrink()
+
+    getRandomDrink()
+
 }
+
 init()
